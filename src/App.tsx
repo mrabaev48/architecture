5fe2d612ff4587ec480acd42527ff937cc910e54
mrabaev48 <mrabaev48@gmail.com>
2023-02-10 22:08:23 +0200

    #6 - Add routing Added: - react-router-dom - MainPage with async - AboutPage with async

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { Suspense } from "react";
 import "./index.scss";
+import { Link, Route, Routes } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
 
 const App = () => {
-  return <div className={"app"}>Hello</div>;
+  return (
+    <div className={"app"}>
+      <Link to={"/"}>Main</Link>
+      <Link to={"/about"}>About</Link>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={"/about"} element={<AboutPageAsync />} />
+          <Route path={"/"} element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
+    </div>
+  );
 };
 
 export default App;
