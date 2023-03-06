86168f032dee4fc23376a7021ac29d1e51fb0337
mrabaev48 <mrabaev48@gmail.com>
2023-03-06 20:22:41 +0200

    #20 Error Boundary - added ErrorBoundary component.

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
+import { BugButton } from 'app/providers/ErrorBoundary';
 
 const MainPage = () => {
     const { t } = useTranslation('mainPage');
-    return <div>{t('mainPage')}</div>;
+    return (
+        <div>
+            <BugButton />
+            {t('mainPage')}
+        </div>
+    );
 };
 
 export default MainPage;
